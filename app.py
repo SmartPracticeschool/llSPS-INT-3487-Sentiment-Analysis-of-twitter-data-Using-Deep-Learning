66bad2f61d0c3886d621ee06868e3f37c6684717
Ananyavajrapu99 <50730268+Ananyavajrapu99@users.noreply.github.com>
2020-07-28 22:46:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, request, jsonify, render_template
+from brain import brain
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    err = "Saved Model Doesn't Exist"
+    if os.path.isfile('./model.h5'): err = ''
+    return render_template('index.html', result='', err=err)
+
+
+@app.route('/', methods=['POST'])
+def y_predict():
+    sentiment = request.form["Message"]
+    err, res = str(brain(sentiment)), ''
+    if err == '0' or err == '1':
+        res, err = err, res
+    return render_template('index.html', result=res + '.png', err=err)
+
+
+if __name__ == "__main__":
+    app.run()
+
